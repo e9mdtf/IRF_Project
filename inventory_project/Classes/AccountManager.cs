24499aa9d5810b528868ea51eb6ea17b40ded102
Áron Vikete <aron.vikete@stud.uni-corvinus.hu>
2020-12-15 09:56:21 +0100

    Account manager osztály hozzáadása

---
+++ inventory_project/Classes/AccountManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_project.Classes
+{
+    class AccountManager
+    {
+        public String accountName { get; set; }
+        public String password { get; set; }
+    }
+}
